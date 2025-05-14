02a315c24d65932409f0ddc531686d8305c66d9b
Jordan Godkin <74619109+BrimmingTeacup@users.noreply.github.com>
2025-05-14 15:13:12 +0000

    completed Pt 9 of Ch 1

--- src/Ch01/01_09b/CodeDemo.cpp
+++ src/Ch01/01_09b/CodeDemo.cpp
@@ -3,9 +3,15 @@
 // Terminal Interaction, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
 
 int main(){
-    std::cout << "Hi There!" << std::endl;
+
+    std::string name; //declares the name variable
+
+    std::cout << "Enter your name: " << std::flush; //flush This will make sure to completely send the output to the terminal before waiting for the user input, without also creating a new line.
+    std::cin >> name; //this requests the user to input their name with a standard input, extracting the information and storing it in the name variable
+    std::cout << "Nice to meet you, " << name << "!" << std::endl; //<< is important so we can concatenate the string and variable together. 
 
     std::cout << std::endl << std::endl;
     return 0;
